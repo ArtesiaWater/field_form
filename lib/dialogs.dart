cf7d53d1d3f9d1f015d16bf5c55e4fbe0b695150
Ruben Caljé <r.calje@artesia-water.nl>
2021-10-18 11:01:53 +0200

    Remove unused import

--- lib/dialogs.dart
+++ lib/dialogs.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void showLoaderDialog(BuildContext context, {String text='Loading...'}) {
   showDialog(barrierDismissible: false,
