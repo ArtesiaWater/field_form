4aa96b247546d68cfa0992ca863bd56af922b81d
Ruben Caljé <r.calje@artesia-water.nl>
2023-05-16 15:29:16 +0200

    make settings optional

--- python/fieldform.py
+++ python/fieldform.py
@@ -53,28 +53,29 @@ def location_file_from_fieldlogger(fname_csv, fname_json, return_data=False):
     # %% settings
     # just copy settings
     # (not all are supported in FieldForm, see https://github.com/ArtesiaWater/field_form)
-    data["settings"] = locs["settings"]
-    # some settings have been renamed:
-    translate_settings = {
-        "ftp_download_hostname": "ftp_hostname",
-        "ftp_download_path": "ftp_path",
-        "ftp_download_username": "ftp_username",
-        "ftp_download_password": "ftp_password",
-    }
-    drop_settings = [
-        "ftp_upload_hostname",
-        "ftp_upload_path",
-        "ftp_upload_username",
-        "ftp_upload_password",
-    ]
-    for fl_setting, ff_setting in translate_settings.items():
-        if fl_setting in data["settings"]:
-            data["settings"][ff_setting] = data["settings"][fl_setting]
-            drop_settings.append(fl_setting)
+    if "settings" in locs:
+        data["settings"] = locs["settings"]
+        # some settings have been renamed:
+        translate_settings = {
+            "ftp_download_hostname": "ftp_hostname",
+            "ftp_download_path": "ftp_path",
+            "ftp_download_username": "ftp_username",
+            "ftp_download_password": "ftp_password",
+        }
+        drop_settings = [
+            "ftp_upload_hostname",
+            "ftp_upload_path",
+            "ftp_upload_username",
+            "ftp_upload_password",
+        ]
+        for fl_setting, ff_setting in translate_settings.items():
+            if fl_setting in data["settings"]:
+                data["settings"][ff_setting] = data["settings"][fl_setting]
+                drop_settings.append(fl_setting)
 
-    for fl_setting in drop_settings:
-        if fl_setting in data["settings"]:
-            data["settings"].pop(fl_setting)
+        for fl_setting in drop_settings:
+            if fl_setting in data["settings"]:
+                data["settings"].pop(fl_setting)
 
     # %% inputfields
     if "inputfields" in locs:
