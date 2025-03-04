377524c3629a79e39a44af873318fbe841d81d79
Ruben Caljé <r.calje@artesia-water.nl>
2025-03-04 12:56:08 +0100

    Update iOS settings

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,15 +2,13 @@ import UIKit
 import Flutter
 import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if let value = ProcessInfo.processInfo.environment["maps_api_key"] {
-        GMSServices.provideAPIKey(value)
-    }
+    GMSServices.provideAPIKey("${mapsApiKey}")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
